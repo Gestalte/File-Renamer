1fb5bfefb4ae8b92970eabdc0d694c2187e63efa
Gestalte <philipbenade@gmail.com>
2020-06-22 21:53:14 +0200

    Planned out how I want the program to work as TODO comments.

--- File-Renamer/Program.cs
+++ File-Renamer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace File_Renamer
 {
@@ -8,5 +9,18 @@ namespace File_Renamer
         {
             Console.WriteLine("Hello World!");
         }
+
+        public static string[] GetAllFiles()
+            => Directory.GetFiles(Directory.GetCurrentDirectory());
+
+        // TODO: Create an Interface for Filter Rules to apply to the files found in the direcory string[] -> string[]
+
+        // TODO: Create an Interface for Rules to apply to fileNames. string -> string
+
+        // TODO: Get all files in the current directory
+
+        // TODO: load all the Filter Rules and apply them to the list of files to filter it.
+
+        // TODO: loop through the filtered list of files and load and apply each rule to the current file's file name then rename it.
     }
 }
