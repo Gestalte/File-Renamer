b8485838b065a16e68301a991a2c4183136a63f5
Gestalte <philipbenade@gmail.com>
2020-06-22 21:43:45 +0200

    Project Creation

---
+++ File-Renamer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace File_Renamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
