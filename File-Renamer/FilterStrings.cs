150f09660f110e5786fd6490415491428591c2c7
Gestalte <philipbenade@gmail.com>
2020-07-02 13:14:08 +0200

    actually adding the files to git this time...

---
+++ File-Renamer/FilterStrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace File_Renamer
+{
+    public class FilterStrings
+    {
+        private readonly List<Filter> filters;
+
+        public FilterStrings(List<Filter> filters)
+        {
+            this.filters = filters ?? throw new ArgumentNullException(nameof(filters));
+        }
+
+        public string[] Filter(string[] inputList)
+        {
+            string[] localList = inputList; // Doesn't mutate array.
+
+            foreach (var filter in filters)
+            {
+                localList = filter.FilterFilepaths(localList);
+            }
+
+            return localList;
+        }
+    }
+}
---
+++ File-Renamer/StringRetriever.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace File_Renamer
+{
+    public class StringRetriever
+    {
+        private readonly IFileProvider fileProvider;
+
+        public StringRetriever(IFileProvider fileProvider)
+        {
+            this.fileProvider = fileProvider ?? throw new ArgumentNullException(nameof(fileProvider));
+        }
+
+        public string[] GetFileList()
+            => fileProvider.GetAllFiles();
+    }
+}
